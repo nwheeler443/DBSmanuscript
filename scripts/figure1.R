74c849971e88fe85692cafc612088d7bf3fccbcc
nwheeler443 <nwheeler443@gmail.com>
2015-07-09 15:25:10 +1200

    updating datafiles and testing scripts

--- scripts/figure1.R
+++ scripts/figure1.R
@@ -1,11 +1,11 @@
 library("ROCR")
 
-lysozyme <- read.delim("lysozymeAUC.txt", header=F)
-laci <- read.delim("lacIAUC.txt", header=F)
-hiv <- read.delim("HIVAUC.txt", header=F)
-human <- read.delim("humanAUC.txt", header=T)
+lysozyme <- read.delim("../data/figure1/auc_data/phage_lysozyme/lysozymeAUC.txt", header=F)
+laci <- read.delim("../data/figure1/auc_data/lacI/lacIAUC.txt", header=F)
+hiv <- read.delim("../data/figure1/auc_data/HIV_protease/HIVAUC.txt", header=F)
+human <- read.delim("../data/figure1/auc_data/humanAUC.txt", header=T)
 
-bestdist <- read.table("custom_lacI.scan", comment.char="#", header=F)
+bestdist <- read.table("../data/figure1/auc_data/lacI/40.scan", comment.char="#", header=F)
 bestdist$dbs <- (bestdist[bestdist$V4=="wild_type",8] - bestdist[,8])
 bestdistpositive <- bestdist[grep("-", bestdist$V4),24]
 bestdistpositive <- cbind(bestdistpositive, rep(1, length(bestdistpositive)))
@@ -15,7 +15,7 @@ bestdistROC <- rbind(bestdistpositive, bestdistnegative)
 bestpred <- prediction(bestdistROC[,1], labels=bestdistROC[,2])
 bestperf <- performance(bestpred, "tpr", "fpr")
 
-lacpfam <- read.delim("Pfam_lacI.txt", header=F, stringsAsFactors=F)
+lacpfam <- read.delim("../data/figure1/auc_data/lacI/scanproc2", header=F, stringsAsFactors=F)
 lacpfam <- lacpfam[order(lacpfam[,1]),]
 lacpfam <- lacpfam[match(bestdist[,4], lacpfam[,1]),]
 lacpfamlabels <- rep(0, nrow(lacpfam))
@@ -24,62 +24,87 @@ lacpfampred <- prediction(lacpfam[,2], labels=lacpfamlabels)
 lacpfamperf <- performance(lacpfampred, "tpr", "fpr")
 lacpfam$name <- lapply(lacpfam[,1], function(x) strsplit(x, "\\W")[[1]][1])
 
-lacprovean <- read.delim("provean_lacI.tsv", header=F, stringsAsFactors=F)
+lacprovean <- read.delim("../data/figure1/auc_data/lacI/provean.tsv", header=F, stringsAsFactors=F)
 lacprovean <- lacprovean[match(lacpfam$name, lacprovean[,1]),]
 lacproveanpred <- prediction((lacprovean[,2]*(-1)), labels=lacpfamlabels)
 lacproveanperf <- performance(lacproveanpred, "tpr", "fpr")
 
-lacpolyphen <- read.delim("polyphen_lacI.tsv", header=F, stringsAsFactors=F)
+lacpolyphen <- read.delim("../data/figure1/auc_data/lacI/polyphen.tsv", header=T, stringsAsFactors=F)
 lacpolyphen <- lacpolyphen[match(lacpfam$name, lacpolyphen[,1]),]
 lacpolyphenpred <- prediction(lacpolyphen[,2], labels=lacpfamlabels)
 lacpolyphenperf <- performance(lacpolyphenpred, "tpr", "fpr")
 
-humanneg <- read.table("treefam_polymorphism_gene_information.txt", header=T)
-humanpos <- read.table("treefam_disease_gene_information.txt", header=T)
+humanneg <- read.table("../data/figure1/treefam_polymorphism_gene_information.txt", header=T)
+humanpos <- read.table("../data/figure1/treefam_disease_gene_information.txt", header=T)
 hneg <- hist(humanneg[,3], breaks=50000)
 hpos <- hist(humanpos[,3], breaks=50000)
 
+# check skewness of polymorphism distribution
+negtrim <- humanneg[abs(humanneg[,3])<sd(humanneg[,3])*4,3]
+negtrim <- negtrim[abs(negtrim)<sd(negtrim)*4]
+negtrim <- negtrim[abs(negtrim)<sd(negtrim)*4]
+negtrim <- negtrim[abs(negtrim)<sd(negtrim)*4]
+negtrim <- negtrim[abs(negtrim)<sd(negtrim)*4]
+negtrim <- negtrim[abs(negtrim)<sd(negtrim)*4]
+negtrim <- negtrim[abs(negtrim)<sd(negtrim)*4]
+negtrim <- negtrim[abs(negtrim)<sd(negtrim)*4]
+negtrim <- negtrim[abs(negtrim)<sd(negtrim)*4]
+negtrim <- negtrim[abs(negtrim)<sd(negtrim)*4]
+negdist <- rnorm(n=length(negtrim), mean=0, sd=sd(negtrim))
+test <- wilcox.test(x=negtrim, y=negdist, paired=T, alternative="less")
+
+plotfig <- function() {
+	par(fig=c(0,1,0.5,1), mar=c(7,5,3,12), cex.main=1.4, cex.axis=1.2)
+	
+	plot(c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100), lysozyme[1:11], lwd=2.5, type="l", main="AUC values for different methods across four benchmarking data sets", col="cadetblue", ylim=c(0.52,0.92), xlim=c(0, 170), xaxt="n", xlab="", ylab="AUC", cex.lab=1.2)
+	lines(c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100), laci[1:11], col="coral4", lwd=2.5)
+	lines(c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100), hiv[1:11], col="antiquewhite4", lwd=2.5)
+	points(c(115, 145, 155), lysozyme[12:14], col="cadetblue", pch=16, cex=1.3)
+	points(c(115, 145, 155), laci[12:14], col="coral4", pch=16, cex=1.3)
+	points(c(115, 145, 155), hiv[12:14], col="antiquewhite4", pch=16, cex=1.3)
+	points(c(115, 125, 145, 155, 165), human[c(12,13,15,16,17)], col="navy", pch=16, cex=1.3)
+	points(c(135,134,135,136), y=c(lysozyme[5], laci[5], hiv[5], human[14]), col=c("cadetblue", "coral4", "antiquewhite4", "navy"), pch=16, cex=1.3)
+	points(165, human[18], col="navy", pch=21, cex=1.3, lwd=3)
+	
+	text(-24,0.98, labels="A", xpd=T, cex=2)
+	
+	axis(1, at=c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100), labels=c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100))
+	axis(1, at=c(115, 125, 135, 145, 155, 165), labels=c("Pfam DBS", "Treefam DBS", "Custom DBS", "PROVEAN", "PolyPhen-2", "FATHMM"), las=2)
+	
+	legend(x=180, y=0.85, cex=1.3, xpd=T,fill=c("cadetblue", "coral4", "antiquewhite4", "navy"), legend=c("Phage lysozyme", "LacI", "HIV protease", "Human variants"), title=expression(bold("Data set")))
+	
+	text(50, 0.35, labels="Percent identity\n(Custom-built HMMs)", xpd=T, cex=1.3)
+	#text(145,0.4, labels="Method", xpd=T, cex=1.5)
+	
+	par(fig=c(0,0.5, 0,0.5), new=T, mar=c(5,5,5,5))
+	
+	plot(bestperf, lwd=2.5, main="ROC curve for LacI\n40%identity model", cex.lab=1.4)
+	plot(lacpfamperf, add=T, lwd=2.5, lty=4)
+	plot(lacproveanperf, add=T, lwd=2.5, lty=2)
+	plot(lacpolyphenperf, add=T, lwd=2.5, lty=3)
+	legend(x=0.5, y=0.5, legend=c("Custom", "PROVEAN", "PolyPhen-2", "Pfam"), lty=c(1,2,3,4), xpd=T, lwd=2.5, cex=1.3)
+	
+	text(-0.26,1.25, labels="B", xpd=T, cex=2)
+	text(1.25,1.25, labels="C", xpd=T, cex=2)
+	
+	par(fig=c(0.5,1,0,0.5), new=T, mar=c(5,5,5,5))
+	
+	plot(density(humanneg[abs(humanneg[,3])<10,3], bw=0.2), xlim=c(-10,10), col="navy", lwd=2.5, main="DBS for human polymorphisms\nTreefam models", cex.lab=1.4, xlab="Delta-bitscore", ylab="Density")
+	lines(density(humanpos[abs(humanpos[,3])<10,3], bw=0.2), col="coral4", lwd=2.5)
+	
+	#plot(hneg$mids, hneg$count, type="l", col="navy", lwd=2.5, xlim=c(-7, 10), main="DBS for human polymorphisms\nTreefam models", cex.lab=1.5, xlab="Delta-bitscore", ylab="Frequency")
+	#lines(hpos$mids, hpos$count, col="coral4", lwd=2.5)
+	legend(x=1, y=0.38, legend=c("Polymorphic", "Disease"), fill=c("navy", "coral4"), xpd=T, cex=1.3)
+	
+	dist <- rnorm(n=nrow(humanneg), m=mean(humanneg[abs(humanneg[,3])<5,3]), sd=sd(humanneg[abs(humanneg$DBS)<5,3]))
+	#lines(density(dist), col="navy", lty=3, lwd=2)
+
+}
+
 png("fig1.png", width=900, height=700)
+plotfig()
+dev.off()
 
-par(fig=c(0,1,0.5,1), mar=c(7,5,3,12), cex.main=1.4, cex.axis=1.2)
-
-plot(c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100), lysozyme[1:11], lwd=2.5, type="l", main="AUC values for different methods across four benchmarking data sets", col="cadetblue", ylim=c(0.52,0.92), xlim=c(0, 170), xaxt="n", xlab="", ylab="AUC", cex.lab=1.2)
-lines(c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100), laci[1:11], col="coral4", lwd=2.5)
-lines(c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100), hiv[1:11], col="antiquewhite4", lwd=2.5)
-points(c(115, 145, 155), lysozyme[12:14], col="cadetblue", pch=16, cex=1.3)
-points(c(115, 145, 155), laci[12:14], col="coral4", pch=16, cex=1.3)
-points(c(115, 145, 155), hiv[12:14], col="antiquewhite4", pch=16, cex=1.3)
-points(c(115, 125, 145, 155, 165), human[c(12,13,15,16,17)], col="navy", pch=16, cex=1.3)
-points(c(135,134,135,136), y=c(lysozyme[5], laci[5], hiv[5], human[14]), col=c("cadetblue", "coral4", "antiquewhite4", "navy"), pch=16, cex=1.3)
-points(165, human[18], col="navy", pch=21, cex=1.3, lwd=3)
-
-text(-24,0.98, labels="A", xpd=T, cex=2)
-
-axis(1, at=c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100), labels=c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100))
-axis(1, at=c(115, 125, 135, 145, 155, 165), labels=c("Pfam DBS", "Treefam DBS", "Custom DBS", "PROVEAN", "PolyPhen-2", "FATHMM"), las=2)
-
-legend(x=180, y=0.85, cex=1.3, xpd=T,fill=c("cadetblue", "coral4", "antiquewhite4", "navy"), legend=c("phage lysozyme", "LacI", "HIV protease", "Human variants"), title=expression(bold("Data set")))
-
-text(50, 0.35, labels="Percent identity\n(Custom-built HMMs)", xpd=T, cex=1.3)
-
-par(fig=c(0,0.5, 0,0.5), new=T, mar=c(5,5,5,5))
-
-plot(bestperf, lwd=2.5, main="ROC curve for LacI\n40%identity model", cex.lab=1.4)
-plot(lacpfamperf, add=T, lwd=2.5, lty=4)
-plot(lacproveanperf, add=T, lwd=2.5, lty=2)
-plot(lacpolyphenperf, add=T, lwd=2.5, lty=3)
-legend(x=0.4, y=0.5, legend=c("Custom", "PROVEAN", "PolyPhen-2", "Pfam"), lty=c(1,2,3,4), xpd=T, lwd=2.5, cex=1.3)
-
-text(-0.26,1.25, labels="B", xpd=T, cex=2)
-text(1.25,1.25, labels="C", xpd=T, cex=2)
-
-par(fig=c(0.5,1,0,0.5), new=T, mar=c(5,5,5,5))
-
-plot(density(humanneg[abs(humanneg[,3])<10,3], bw=0.2), xlim=c(-10,10), col="navy", lwd=2.5, main="DBS for human polymorphisms\nTreefam models", cex.lab=1.4, xlab="Delta-bitscore", ylab="Density")
-lines(density(humanpos[abs(humanpos[,3])<10,3], bw=0.2), col="coral4", lwd=2.5)
-
-legend(x=1, y=0.38, legend=c("Polymorphic", "Disease"), fill=c("navy", "coral4"), xpd=T, cex=1.3)
-
-dist <- rnorm(n=nrow(humanneg), m=mean(humanneg[abs(humanneg[,3])<5,3]), sd=sd(humanneg[abs(humanneg$DBS)<5,3]))
-
+pdf("fig1.pdf", width=9, height=7)
+plotfig()
 dev.off()
--- scripts/figure2.R
+++ scripts/figure2.R
@@ -1,4 +1,5 @@
-dbs <- read.delim("ent-gal.dbs", comment.char="#", header=F, stringsAsFactors=F)
+dbs <- read.delim("../data/figure2/ent-gal.dbs", comment.char="#", header=F, stringsAsFactors=F)
+exclude = read.table("../data/figure2/nonschanges")
 
 dbsgenes <- unique(dbs[,1])
 scores = data.frame()
@@ -6,7 +7,7 @@ for (i in 1:length(dbsgenes)) {
     scores[i,1] = sum(dbs[!is.na(match(dbs[,1], dbsgenes[i])),10])
 }
 
-dnds <- read.table("dndsvalues.tsv", header=TRUE)
+dnds <- read.table("../data/figure2/dndsvalues.tsv", header=TRUE)
 
 dist <- rnorm(n=50000, mean=0, sd=2.13290575206885)
 distdens <- density(dist, bw=0.3)
@@ -18,21 +19,19 @@ combined <- combined[combined[,6]>0 & combined$Ka>0 & combined$Ks>0,]
 redcolour <- rgb(170,0,0,70, max=255)
 bluecolour <- rgb(70, 120, 190,70, max=255)
 
-pathdata <- read.table("pathloss.genes.unique.txt", header = T, sep = "\t")
+pathdata<-read.table("../data/figure2/pathloss.genes.unique.txt", header = T, sep = "\t")
 pathdata <- pathdata[c(1,2,3,4,5,6,8,11,16,17,19,21),]
 pathdata <- pathdata[nrow(pathdata):1,]
 
 pathdata$totalgenes <- pathdata[,1]*2+(pathdata[,4]+pathdata[,5])*2
-pathdata[,7:10] <- pathdata[,2:5]/pathdata$totalgenes						# losses and uniques as fractions of total
-pathdata[,11:12] <- (pathdata[,1]-pathdata[,2:3]+pathdata[,4:5])/pathdata[,6]			# functional proportion of genome
+pathdata[,9:14] <- pathdata[,2:7]/pathdata$totalgenes						# losses and uniques as fractions of total
+pathdata[,15:16] <- (pathdata[,1]-pathdata[,2:3]-pathdata[6:7]+pathdata[,4:5])/pathdata[,8]			# functional proportion of genome
 
-pathdata <- pathdata[pathdata[,12]!="Inf",]
-pathdataT<-matrix(c(pathdata[,10], pathdata[,7], pathdata[,11], pathdata[,12], pathdata[,8], pathdata[,9]),nrow=nrow(pathdata),ncol=6,dimnames=list(c(as.character(rownames(pathdata))),c("unique to pathogen", "loss in commensal", "commensal", "pathogen", "loss in pathogen", "unique to commensal")))
+pathdataT<-matrix(c(pathdata[,12], pathdata[,13], pathdata[,9], pathdata[,15], pathdata[,16], pathdata[,10], pathdata[,14], pathdata[,11]),nrow=nrow(pathdata),ncol=8,dimnames=list(c(as.character(rownames(pathdata))),c("unique to Gallinarum", "N and B loss in Enteritidis", "DBS loss in Enteritidis", "Enteritidis functional", "Gallinarum functional", "DBS loss in Gallinarum", "N and B loss in Gallinarum", "unique to Enteritidis")))
 
 simpleCap <- function(x) {
 	s <- strsplit(x, " ")[[1]]
-	paste(toupper(substring(s, 1,1)), substring(s, 2),
-	sep="", collapse=" ")
+	paste(toupper(substring(s, 1,1)), substring(s, 2), sep="", collapse=" ")
 }
 
 dbshist <- hist(log(combined[,6]+0.01), breaks=15)
@@ -41,6 +40,7 @@ dndshist <- hist(log10(combined[,5]), breaks=50)
 ################################################################################
 
 plotfig <- function() {
+	#####################
 	
 	par(fig=c(0,0.78,0.7,1), mar=c(3.5,9,2,1), mgp=c(2,0.5,0))
     hist(scores[is.na(match(dbsgenes, exclude[,1])),1], xlab="Delta-bitscore", main="",xlim=c(-8,8), breaks=300, freq=FALSE, ylim=c(0,0.5))
@@ -49,21 +49,25 @@ plotfig <- function() {
 	
 	###################
 	
-	op<-par(fig=c(0,1,0.4,0.7), las=2, mar=c(0,14,1,16.5), xpd=T, new=TRUE)			# mfrow for mulit-panelled plot
-	bp <- barplot(t(pathdataT) , col=c(rgb(30, 30, 100, max=255), rgb(70, 95, 190, max=255), rgb(130, 160, 255, 200,max=255), rgb(205, 82, 82,200, max=255), rgb(145, 42, 42,240, max=255), rgb(100, 0, 0, max=255)),horiz=TRUE, xaxt="n")
+	op<-par(fig=c(0,1,0.4,0.7), las=2, mar=c(0,12,1,14), xpd=T, new=TRUE)			# mfrow for mulit-panelled plot
+	bp <- barplot(t(pathdataT) ,
+	col=c(rgb(10, 10, 50, max=255), rgb(50, 70, 150, max=255), rgb(50, 70, 170, 210, max=255), rgb(70, 120, 190,80, max=255),
+	rgb(170,30,0,80, max=255), rgb(180, 50, 50, 210, max=255), rgb(150, 30, 30, max=255), rgb(50, 0, 0, max=255)),
+	, horiz=TRUE, cex.lab=0.2, xaxt="n", cex.names=1.1, cex.main=1.5)
 	
-	text(x=c(rep(.40, nrow(pathdataT)), rep(.60, nrow(pathdataT))), y=bp, labels=c(pathdata[,6]/2-pathdata[,5]-pathdata[,2],pathdata[,6]/2-pathdata[,4]-pathdata[,3]))
+	text(x=c(rep(.40, nrow(pathdataT)), rep(.60, nrow(pathdataT))), y=bp, labels=c(pathdata[,8]/2-pathdata[,5]-pathdata[,6]-pathdata[,2],pathdata[,8]/2-pathdata[,4]-pathdata[,7]-pathdata[,3]))
 	
-	legend(title="Classification of genes", x=1.1, y=nrow(pathdata), c("absent in Enteritidis", "loss of function in Enteritidis", "functional in Enteritidis", "functional in Gallinarum", "loss of function in Gallinarum", "absent in Gallinarum"), fill=c(rgb(30, 30, 100, max=255), rgb(70, 95, 190, max=255), rgb(130, 160, 255, max=255), rgb(205, 82, 82, max=255), rgb(145, 42, 42, max=255), rgb(100, 0, 0, max=255)))
+	legend(title="Classification of genes", x=1.03, y=nrow(pathdata), c("Absent in Enteritidis", "Nuccio and Bäumler HDC", "Low DBS", "Functional in Enteritidis", "Functional in Gallinarum", "High DBS", "Nuccio and Bäumler HDC", "Absent in Gallinarum"), fill=c(rgb(10, 10, 50, max=255), rgb(50, 70, 150, max=255), rgb(50, 70, 170, 210, max=255), rgb(70, 120, 190,80, max=255),
+	rgb(170,30,0,80, max=255), rgb(180, 50, 50, 210, max=255), rgb(150, 30, 30, max=255), rgb(50, 0, 0, max=255)))
 	text(x=c(.25,.75), y=nrow(pathdataT)+3, labels=c("Enteritidis", "Gallinarum"), font=2)
-	text(y=nrow(pathdataT)+3, x=-0.7, labels="B", font=2, cex=1.4)
+	text(y=nrow(pathdataT)+3.3, x=-0.47, labels="B", font=2, cex=1.4)
 	
 	####################
 	
 	par(fig=c(0,0.93,0,0.36), mar=c(4,5,2,3), new=T, mgp=c(2.5,0.7,0))
 	plot(combined[,5], abs(combined[,6])+0.01, xlab=expression(bold(paste("d"[N],"/d"[S]))), ylab=expression(bold(paste("|DBS|"))), pch=20, log="xy", xaxt="n", yaxt="n", cex.lab=1.2)
-	axis(side=1, at=c(0.01,0.1,1,10,100), labels=c(0,0.1,1,10,100), cex.axis=1, las=0)
-	axis(side=2, at=c(0.01,0.1,1,10,100), labels=c(0,0.1,1,10,100), cex.axis=1)
+	axis(side=1, at=c(0.01,0.1,1,10,100), labels=c(0.01,0.1,1,10,100), cex.axis=1, las=0)
+	axis(side=2, at=c(0.01,0.1,1,10,100), labels=c(0.01,0.1,1,10,100), cex.axis=1)
 	
 	par(xpd=F)
 	
@@ -105,5 +109,4 @@ plotfig()
 dev.off()
 
 cor(abs(combined[,6]), combined$KaKs, method="spearman")
-cor(abs(combined[,6]), combined$Ka, method="spearman")
 
